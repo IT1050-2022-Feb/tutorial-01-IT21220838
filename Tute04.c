343ae8c713c6b0d31b89608ec6f922b451840bf0
IT21220838 <99346436+IT21220838@users.noreply.github.com>
2022-02-09 16:22:34 +0000

    Tutorial 04 -1st week

--- Tute04.c
+++ Tute04.c
@@ -4,16 +4,62 @@ Implement the three functions minimum(), maximum() and multiply() below the main
 
 Do not change the code given in the main() function when you are implementing your solution.*/
 
-#include <stdio.h>
+#include<stdio.h>
+int maximum(int a,int b);
+int minimum(int p,int q);
+int multiply(int r,int s);
 
-int main() {
-   int no1, no2;
-   printf("Enter a value for no 1 : ");
-   scanf("%d", &no1);
-   printf("Enter a value for no 2 : ");
-   scanf("%d", &no2);
-   printf("%d ", minimum(no1, no2));
-   printf("%d ", maximum(no1, no2));
-   printf("%d ", multiply(no1, no2));
-   return 0;
+int main(void)
+{
+	int no1,no2;
+	
+	printf("Enter a value for no 1 : "); 
+    scanf("%d", &no1);
+    
+    printf("Enter a value for no 2 : "); 
+    scanf("%d", &no2);
+
+    printf("%d ", minimum(no1, no2)); 
+     printf("%d ", maximum(no1, no2)); 
+     printf("%d ", multiply(no1, no2)); 
+ 
+ 
+ return 0;
+}
+
+int maximum(int a,int b)
+{
+	int max;
+	
+	if(a<b)
+	max = b;
+	
+	else
+	max = a;
+	
+	return max;	
+}
+
+int minimum(int p,int q)
+{
+	int min;
+	
+	if(p>q)
+	min = q;
+	
+	else
+	min = p;
+	
+	return min;
+	
+}
+
+int multiply(int r,int s)
+{
+	int multy;
+	
+	multy = r * s ;
+	
+	return multy;
+	
 }
